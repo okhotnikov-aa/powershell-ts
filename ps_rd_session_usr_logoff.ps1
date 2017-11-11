b36aec7e4bb656d176c40a226be75354529ea87e
okhotnikov-aa <33516098+okhotnikov-aa@users.noreply.github.com>
2017-11-11 16:54:49 +0300

    PS script to logoff dissconnected sessions
    PS script to logoff dissconnected sessions

---
+++ ps_rd_session_usr_logoff.ps1
@@ -0,0 +1,6 @@
+﻿$RDSessions = Get-RDUserSession -ConnectionBroker ld-s-rdcb03n02.ao.nlmk | Where-Object -Filter {$_.SessionState -eq 'STATE_DISCONNECTED'}
+ Foreach ($RDSession in $RDSessions)
+ {
+ Invoke-RDUserLogoff -UnifiedSessionID $RDSession.SessionId -HostServer $RDSession.HostServer -Force
+ Write-Host "The User" $RDSession.UserName "is logged off from" $RDSession.HostServer "server"
+ } 
