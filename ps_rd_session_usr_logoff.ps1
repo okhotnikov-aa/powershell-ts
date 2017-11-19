7e74b24d2eac20ddbd11658c42b2fbbe07385f81
okhotnikov-aa <33516098+okhotnikov-aa@users.noreply.github.com>
2017-11-19 19:45:00 +0300

    RD Deployment info

--- ps_rd_session_usr_logoff.ps1
+++ ps_rd_session_usr_logoff.ps1
@@ -1,22 +1,29 @@
-﻿ Write-Host "RDS Util by okhotnikov-aa"
- Write-Host ""
- Write-Host " 1. Log Off Disconnected Sessions"
- Write-Host " 2. Log Of All Sessions"
- Write-Host " 0. Quit"
- $menu = Read-Host ""
+﻿Write-Host ""
+Write-Host "RDS User Control"
+Write-Host ""
+Write-Host "Please enter RD Connection Broker FQDN:"
+$rdcb = "ld-s-rdcb03n02.ao.nlmk" #Read-Host 
+Write-Host ""
 
- Write-Host ""
- Write-Host "Please enter RD Connection Broker FQDN"
- $rdcb = Read-Host ""
+$rddepl = Get-RDServer -ConnectionBroker $rdcb | Where-Object -Filter {$_.Roles -eq 'RDS-RD-SERVER'} # get all RDSH in deployment
+$sessions = Get-RDUserSession -ConnectionBroker $rdcb # get all sessions in deployment
+Write-Host "This RD Deployment contains:"
+Write-Host ""
+foreach ($rddepl in $rddepl) # show all RDSH and they connected sessions
+{
+    Write-Host $rddepl.Server
 
- Switch ($menu) {
- "1" {$session_list = Get-RDUserSession -ConnectionBroker $rdcb | Where-Object -Filter {$_.SessionState -eq 'STATE_DISCONNECTED'} }
- "2" {$session_list = Get-RDUserSession -ConnectionBroker $rdcb}
- "0" {Exit}
- }
+    #cout all sessions
+    $ssns = $sessions | Where-Object -Filter {$_.HostServer -eq $rddepl.Server} | measure
+    Write-Host "Number of sessions:" $ssns.Count 
 
- Foreach ($session_list in $session_list)
- {
- Invoke-RDUserLogoff -UnifiedSessionID $session_list.SessionId -HostServer $session_list.HostServer -Force
- Write-Host "The User" $session_list.UserName "is succesfully logged off from" $session_list.HostServer "server"
- } 
+    #count active sessions
+    $ssns = $sessions | Where-Object -Filter {$_.HostServer -eq $rddepl.Server}| Where-Object -Filter {$_.SessionState -eq 'STATE_ACTIVE'}  | measure
+    Write-Host "Number of active:" $ssnss.Count
+
+    #count disconnected sessions
+    $ssns = $sessions | Where-Object -Filter {$_.HostServer -eq $rddepl.Server} | Where-Object -Filter {$_.SessionState -eq 'STATE_DISCONNECTED'} | measure
+    Write-Host "Number of disconnected:" $ssns.Count
+    Write-Host ""
+
+}
